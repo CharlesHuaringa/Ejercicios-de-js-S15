0ceb0c1e3fb014046d1ac62bae51ded4506006be
CharlesHuaringa <charles.huaringa@gmail.com>
2022-11-16 17:49:31 -0500

    ejerciciosS15

---
+++ ejercicio1.js
@@ -0,0 +1,49 @@
+//  let compra = +prompt("Ingrese el costo de compra")
+// if(compra >= 150 ){
+//     alert(`Recibes un descuento del 12% de ${compra * 12 / 100} soles, El nuevo precio es de ${(compra - (compra* 12 / 100))} soles`)
+// }else{
+//     alert("El costo no es mayor a 150 no recibirá descuento")
+// }
+
+
+// let nombre = +prompt("Ingrese el nombred el alumno")
+// let notaparcial = +prompt("Ingrese su nota parcial")
+// let exmanenfinal = +prompt("Ingrese su nota del examen final")
+// let promediopracticas = +prompt("Ingrese su nota de promedio de practicas")
+// promedio = (notaparcial + exmanenfinal + exmanenfinal + promediopracticas) / 4
+// if(promedio >= 20){
+//     alert(` ${nombre} Estas aprobado con ${promedio}`)
+// } else{
+//     alert("Estas reprobado")
+// }
+
+
+// let nombre = prompt("Ingrese su nombre")
+// let sueldo = +prompt("Ingrese su sueldo básico")
+// let hijos = +prompt("Ingrese la cantidad de hijos que tiene")
+// bonificación = sueldo * 0.07
+// sueldofinal = sueldo + bonificación
+// if(hijos > 0){
+//     alert(`El sueldo fijo del 7% es ${bonificación} y el sueldo final es ${sueldofinal}`)
+// } else{
+//     alert(`no posee ningun hijo no recibe bonificación, y el sueldo final es ${sueldo}`)
+// }
+
+
+
+// let numero = prompt("Ingresa un número")
+// if(numero >= 500){
+//     alert(`El 18% de dicho número es ${numero * 0.18}`)
+// }
+// else{
+//     alert("El número ingresado no es mayor a 500")
+// }
+
+
+// let numero = +prompt("Ingrese un número")
+// let total = 1;
+
+// for(let i = 1; i <= numero; i++) {
+//     total = total * i
+// }
+// alert(`El factorial del número ${numero} es ${total}`)
